288074af1d37fd31da0df54ace97fb04a215b1f8
Mikolajtom <nikogamingg@gmail.com>
2024-03-25 16:11:06 +0100

    Test metody join game z controllera HomeController

--- Checkers.Tests/UnitTest1.cs
+++ Checkers.Tests/UnitTest1.cs
@@ -1,8 +1,11 @@
 using Checkers.Controllers;
 using Checkers.Domain.Entities;
 using Checkers.Domain.Interfaces;
+using Checkers.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System.Web.Mvc;
+using System.Xml.Linq;
+
 
 namespace ControllerTests
 {
@@ -22,5 +25,33 @@ namespace ControllerTests
             // Assert
             Assert.NotNull(result);
         }
+
+        [Fact]
+        public async Task JoinGame_ReturnsViewResult()
+        {
+            // Arrange
+            var gameRepository = new Mock<IGameRepository>();
+            var controller = new HomeController(gameRepository.Object);
+            var gameId = "1234";
+            var p2name = "4321";
+            var request = new RequestViewModel()
+            {
+                GameId = gameId,
+                Method = RequestMethods.join,
+                P2Name = p2name
+            };
+
+            // Act
+            var result = controller.JoinGame(gameId, p2name) as ViewResult;
+
+            // Assert
+            Assert.Equal("GameRoom", result.ViewName);
+            var model = result.ViewData.Model as RequestViewModel;
+            Assert.Equal(request.GameId, model.GameId);
+            Assert.Equal(request.P2Name, model.P2Name);
+        }
     }
+
+
 }
+
