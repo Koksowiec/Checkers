795f52ebe7cb71fafa38b76ffad5155a8702d745
Mikolajtom <nikogamingg@gmail.com>
2024-03-25 15:11:19 +0100

    Stworzyłem 1 test HomeController

--- Checkers.Tests/UnitTest1.cs
+++ Checkers.Tests/UnitTest1.cs
@@ -1,11 +1,26 @@
-namespace Checkers.Tests
+using Checkers.Controllers;
+using Checkers.Domain.Entities;
+using Checkers.Domain.Interfaces;
+using Moq;
+using System.Web.Mvc;
+
+namespace ControllerTests
 {
-    public class UnitTest1
+    public class HomeControllerTests
     {
         [Fact]
-        public void Test1()
+        public async Task Index_ReturnsViewResult()
         {
+            // Arrange
+            var gameRepository = new Mock<IGameRepository>();
+            var controller = new HomeController(gameRepository.Object);
+            gameRepository.Setup(g => g.GetAllAsync()).ReturnsAsync(new List<Game>() {new Game() });
 
+            // Act
+            var result = await controller.Index() ;
+
+            // Assert
+            Assert.NotNull(result);
         }
     }
 }
